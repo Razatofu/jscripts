275075d8c0ee49bef18040c4b3ddf5fe33c68f87
raza ayaz <razaayaz@yahoo.com>
2024-12-22 00:11:56 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,9 @@
+alert("Error ! Please entre a valid password ");
+alert("welcome to java land\nhappy coding");
+
+alert('Your message goes here!');
+var userName = false
+console.log(userName);
+document.write(userName);
+alert("userName")
+alert(userName)
